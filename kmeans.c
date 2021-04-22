afac53334274eaf9662c42ee48cc3d439d8a9cc4
Daniel Nekrassov <nekrassov@mail.tau.ac.il>
2021-04-22 10:20:10 +0300

    Started working on main

--- kmeans.c
+++ kmeans.c
@@ -1,16 +1,52 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
-int kMeans(int k, char *filename, int max_iter);
+#define MAX_ITER(x) = (min((x),(200))
 
-int main() {
-    printf("Hello, World!\n");
+#define ASSERT(expr, message) \
+    if (!(expr)){             \
+         printf("%s", message); \
+         exit(0);\
+    } \
+
+
+void kMeans(int k, char *filename, int max_iter);
+
+int main(int argc, char *argv[]) {
+    /* Validating command-line inputs */
+    int k, max_iter, i, d;
+    char* firstRow;
+
+    ASSERT(argc <= 1 , "Not enough arguments");
+
+    k = atoi(argv[0]);
+    max_iter = atoi(argv[1]);
+
+    ASSERT(max_iter > 0 , "max_iter input needs to be positive");
+    ASSERT(k > 0 , "k input needs to be positive");
+
+
+    /* Allocating all needed space for computations - and reallocating if needed */
+
+    /* Finding d based on first row - by number of commas + 1. */
+    firstRow = sscanf("%s", (const char *) stdin);
+    for (i=0, d=1; firstRow[i]; i++){
+        d += (firstRow[i] == ',');
+    }
+
+
+
+
+    /* Calculations */
+    /* Output & Free space */
     return 0;
 }
 
 
 
-int kMeans(int k, char *filename, int max_iter) {
-    int lines, d = 1;
+void kMeans(int k, char *filename, int max_iter) {
+   /* int lines, d = 1;
     char c;
     FILE *in_file = fopen(filename, "r");
 
@@ -26,5 +62,5 @@ int kMeans(int k, char *filename, int max_iter) {
         //create an array of pointers of size LINES with pointer from each cell into array of type float of size (d+1) * size of (float)
 
         return 0;
-    }
+    }*/
 }
