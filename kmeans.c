f2b60be1957ecafb4b3b846190634476e0a38d26
Daniel Nekrassov <nekrassov@mail.tau.ac.il>
2021-04-22 19:58:13 +0300

    v1.0.0.4 - validation part complete, input into main array complete

--- kmeans.c
+++ kmeans.c
@@ -5,6 +5,7 @@
 
 #define MAX_ITER(x) (((x) > (200)) ? (200) : (x))
 #define BUFFER_SIZE 200
+#define INITIAL_ARRAY_SIZE 100
 
 #define ASSERT(expr, message) \
     if (!(expr)){             \
@@ -15,12 +16,15 @@
 
 void kMeans(int k, char *filename, int max_iter);
 
+int initializeArray(int arraySize, float **array, char currRow[], int d);
+
 int main(int argc, char *argv[]) {
     /* Validating command-line inputs */
-    int k, max_iter, i, d, size, tail;
+    int k, max_iter, i, d, arraySize;
     char firstRow[ BUFFER_SIZE ];
-    char *token;
-    float *array;
+
+    float *datap_array, **datapoint, *centr_array, **centroids;
+    int *datap_cluster_assignment;
 
     ASSERT(argc > 1 , "Not enough arguments")
 
@@ -36,32 +40,41 @@ int main(int argc, char *argv[]) {
     for (i=0, d=1; firstRow[i]; i++){
         d += (firstRow[i] == ',');
     }
-    size = 100, tail = 0;
 
-    array = (float*) calloc(d * size, sizeof (float));
+    /*Setting up the datapoint array */
+    arraySize = INITIAL_ARRAY_SIZE;
 
-    do{/* as long as there are still lines to be read */
-       /*loop to read line */
-       token = strtok(firstRow, ","); /* read the current line */
-       while(token != NULL) {
-           array[tail] = (float) atof(token);
-           token = strtok(NULL, ",");
-           tail++;
+    datap_array = (float*) calloc(d * arraySize, sizeof (float));
+    ASSERT(datap_array != NULL, "Error in memory allocation")
 
-           /*in-case we reached the edge of the current array, increase the size by twice */
-           if (tail == d * size){
-               size *= 2;
-               array = realloc(array, d * size * sizeof(float));
-               ASSERT(array != NULL, "Error in memory allocation")
-           }
-       }
-    } while(fgets(firstRow, BUFFER_SIZE, stdin));
+    arraySize = initializeArray(arraySize, &datap_array, firstRow, d);
 
-    /* cut the array to its intended size */
-    if(tail < d * size - 1)  {
-        array = realloc(array, tail * sizeof(float));
-        ASSERT(array != NULL, "Error in memory allocation")
+    /*making datapoint a 2D array and initializing datap_cluster_assignment*/
+    datapoint = calloc(arraySize, sizeof(int*));
+    datap_cluster_assignment = calloc(arraySize, sizeof(int));
+    for (i = 0; i < arraySize * d ; i++){
+        if (i % d == 0){
+            datapoint[i/d] = datap_array + i;
+        }
+        datap_cluster_assignment[i] = 0;
     }
+
+    /* creating initial centroids */
+    centr_array = calloc(k, d * sizeof(float ));
+    ASSERT(centr_array != NULL, "Error in memory allocation")
+
+    for (i = 0; i < k*d; i++){
+        centr_array[i] = datap_array[i];
+    }
+
+    /*making centroids a 2D array*/
+    centroids = calloc(k, sizeof(int*));
+    for (i = 0; i < k ; i++){
+        centroids[i] = centr_array + i * d;
+    }
+
+
+
 
 
     /* Calculations */
@@ -92,4 +105,35 @@ void kMeans(int k, char *filename, int max_iter) {
 
         return 0;
     }*/
+}
+
+int initializeArray(int arraySize, float **array, char currRow[], int d) {
+    char *token;
+    int tail = 0;
+
+    do{/* as long as there are still lines to be read */
+        /*loop to read line */
+        token = strtok(currRow, ","); /* read the current line */
+        while(token != NULL) {
+            (*array)[tail] = (float) atof(token);
+            token = strtok(NULL, ",");
+            tail++;
+
+            /*in-case we reached the edge of the current datap_array, increase the arraySize by twice */
+            if (tail == d * arraySize){
+                arraySize *= 2;
+                *array = realloc(*array, d * arraySize * sizeof(float));
+                ASSERT(*array != NULL, "Error in memory allocation")
+            }
+        }
+    } while(fgets(currRow, BUFFER_SIZE, stdin));
+
+    /* cut the datap_array to its intended arraySize */
+    if(tail < d * arraySize - 1)  {
+        *array = realloc(*array, tail * sizeof(float));
+        ASSERT(*array != NULL, "Error in memory allocation")
+        arraySize = tail / d;
+    }
+
+    return arraySize;
 }
