70d67a8afd82d388373045781b43580fa74ac124
Daniel Nekrassov <nekrassov@mail.tau.ac.il>
2021-04-23 15:19:18 +0300

    v1.0.0.5 - first calculation of clusters - successful

--- kmeans.c
+++ kmeans.c
@@ -6,6 +6,7 @@
 #define MAX_ITER(x) (((x) > (200)) ? (200) : (x))
 #define BUFFER_SIZE 200
 #define INITIAL_ARRAY_SIZE 100
+#define FLT_MAX 3.402823466e+38F
 
 #define ASSERT(expr, message) \
     if (!(expr)){             \
@@ -18,15 +19,24 @@ void kMeans(int k, char *filename, int max_iter);
 
 int initializeArray(int arraySize, float **array, char currRow[], int d);
 
+void updateCentroidsPerDatap(float ***datapoint, float ***centroid, int **datap_cluster_assignment, int d , int k, int size);
+
 int main(int argc, char *argv[]) {
     /* Validating command-line inputs */
     int k, max_iter, i, d, arraySize;
     char firstRow[ BUFFER_SIZE ];
 
-    float *datap_array, **datapoint, *centr_array, **centroids;
+    float *datap_array, **datapoint, *centr_array, **centroid;
     int *datap_cluster_assignment;
 
-    ASSERT(argc > 1 , "Not enough arguments")
+    /* This line helps with printf buffering, since it can be junky sometimes */
+    setvbuf (stdout, NULL, _IONBF, 0);
+
+    ASSERT(argc > 0 , "Not enough arguments")
+
+    if (argc == 1){
+        max_iter = 200;
+    }
 
     k = atoi(argv[1]);
     max_iter = atoi(argv[2]);
@@ -51,34 +61,42 @@ int main(int argc, char *argv[]) {
 
     /*making datapoint a 2D array and initializing datap_cluster_assignment*/
     datapoint = calloc(arraySize, sizeof(int*));
+    ASSERT(datapoint != NULL, "Error in memory allocation")
     datap_cluster_assignment = calloc(arraySize, sizeof(int));
-    for (i = 0; i < arraySize * d ; i++){
-        if (i % d == 0){
-            datapoint[i/d] = datap_array + i;
-        }
+    ASSERT(datap_cluster_assignment != NULL, "Error in memory allocation")
+    for (i = 0; i < arraySize ; i++){
+        datapoint[i] = datap_array + i*d;
         datap_cluster_assignment[i] = 0;
     }
 
-    /* creating initial centroids */
-    centr_array = calloc(k, d * sizeof(float ));
+    /* creating initial centroid */
+    centr_array = (float *) malloc(k * d * sizeof(float ));
     ASSERT(centr_array != NULL, "Error in memory allocation")
-
     for (i = 0; i < k*d; i++){
         centr_array[i] = datap_array[i];
     }
 
-    /*making centroids a 2D array*/
-    centroids = calloc(k, sizeof(int*));
+    /*making centroid a 2D array*/
+    centroid = (float **) calloc(k, sizeof(int*));
+    ASSERT(centroid != NULL, "Error in memory allocation")
     for (i = 0; i < k ; i++){
-        centroids[i] = centr_array + i * d;
+        centroid[i] = centr_array + i * d;
     }
 
-
-
-
-
     /* Calculations */
+
+    updateCentroidsPerDatap(&datapoint, &centroid, &datap_cluster_assignment, d, k, arraySize);
+
+
+
     /* Output & Free space */
+
+    fflush(stdout);
+    free(datap_array);
+    free(datapoint);
+    free(centr_array);
+    free(centroid);
+    free(datap_cluster_assignment);
    return 0;
 }
 
@@ -136,4 +154,26 @@ int initializeArray(int arraySize, float **array, char currRow[], int d) {
     }
 
     return arraySize;
+}
+
+void updateCentroidsPerDatap(float ***datapoint, float ***centroid, int **datap_cluster_assignment, int d, int k, int size){
+    /* For each point i, we shall calculate euclidean distance from him to each cluster j, and find min, and assign to datap_cluster_assignment */
+    int i, j, v, min_cluster;
+    float dist, min_dist;
+    for (i = 0 ; i < size ; i++){
+        min_dist = FLT_MAX, min_cluster = -1;
+
+        for (j = 0; j < k; j++ ){
+            dist = 0;
+            for (v = 0; v < d; v++){
+                dist += ((*datapoint)[i][v] - (*centroid)[j][v])*((*datapoint)[i][v] - (*centroid)[j][v]);
+            }
+
+            if (min_dist > dist){
+                min_dist = dist;
+                min_cluster = j;
+            }
+        }
+        (*datap_cluster_assignment)[i] = min_cluster;
+    }
 }
